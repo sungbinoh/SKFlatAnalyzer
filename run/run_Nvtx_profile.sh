82f3e80d787b08115872b3e26d3797d21c64d145
sungbin oh <sungbin.oh@cern.ch>
2018-11-16 09:41:10 +0900

    adding 949cand2_2

--- run/run_Nvtx_profile.sh
+++ run/run_Nvtx_profile.sh
@@ -1,8 +1,8 @@
 #MC
-python python/SKFlat.py -a NvtxProfile -i DYJets -n 300 &
-#python python/SKFlat.py -a NvtxProfile -l ./run/MC_big.txt -n 2 &
-#python python/SKFlat.py -a NvtxProfile -l ./run/MC_small.txt -n 300 &
-#python python/SKFlat.py -a NvtxProfile -l ./run/MC_minor.txt -n 30 &
+#python python/SKFlat.py -a NvtxProfile -i DYJets -n 300 &
+python python/SKFlat.py -a NvtxProfile -l ./run/MC_big.txt -n 2 &
+python python/SKFlat.py -a NvtxProfile -l ./run/MC_small.txt -n 300 &
+python python/SKFlat.py -a NvtxProfile -l ./run/MC_minor.txt -n 30 &
 
 #DATA
 #python python/SKFlat.py -a NvtxProfile -i SingleMuon -n 200 &
