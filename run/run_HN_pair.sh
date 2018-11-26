41df93ff2b407f23b4b75ba64a97bb2e1e590451
Sungbin Oh <sungbin.oh@cern.ch>
2018-11-26 18:53:37 +0900

    ahah

--- run/run_HN_pair.sh
+++ run/run_HN_pair.sh
@@ -1,6 +1,7 @@
 #Signal (2017)
-#python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_MM.txt -n 2
-python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_EE.txt -q longq -n 1
+python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_MM.txt -q fastq -n 1
+python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_EE.txt -q fastq -n 1
+#python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_EE.txt -q longq -n 1
 
 #MC
 #python python/SKFlat.py -a HN_pair_all -l ./run/MC_big.txt -n 400 &
