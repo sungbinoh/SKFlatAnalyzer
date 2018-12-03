b4947f968c45ba21f9e6310e67193f3a81e2cb57
sungbin oh <sungbin.oh@cern.ch>
2018-12-03 19:11:38 +0900

    fixed data not to smear/scale muon momentum

--- run/run_HN_pair.sh
+++ run/run_HN_pair.sh
@@ -3,12 +3,12 @@
 #python python/SKFlat.py -a HN_pair_all -l ./run/HN_pair_EE.txt -n 20
 
 #MC
-#python python/SKFlat.py -a HN_pair_all -l ./run/MC_big.txt -n 400 &
-#python python/SKFlat.py -a HN_pair_all -l ./run/MC_small.txt -n 300 &
-#python python/SKFlat.py -a HN_pair_all -l ./run/MC_minor.txt -n 30 &
+python python/SKFlat.py -a HN_pair_all -l ./run/MC_big.txt -n 400 &
+python python/SKFlat.py -a HN_pair_all -l ./run/MC_small.txt -n 300 &
+python python/SKFlat.py -a HN_pair_all -l ./run/MC_minor.txt -n 30 &
 #DATA
-#python python/SKFlat.py -a HN_pair_all -i SingleMuon -n 200 &
-#python python/SKFlat.py -a HN_pair_all -i DoubleEG -n 200 &
+python python/SKFlat.py -a HN_pair_all -i SingleMuon -n 200 &
+python python/SKFlat.py -a HN_pair_all -i DoubleEG -n 200 &
 
 #TEST
-python python/SKFlat.py -a HN_pair_all -i DYJets -n 200 &
+#python python/SKFlat.py -a HN_pair_all -i DYJets -n 200 &
